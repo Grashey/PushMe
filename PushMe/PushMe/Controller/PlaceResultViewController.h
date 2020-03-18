482a849bbb2a66e96d796608db52e66ef52eaffb
Aleksandr Fetisov <grashey@mail.ru>
2020-03-18 19:57:34 +0300

    renamed files

---
+++ PushMe/PushMe/Controller/PlaceResultViewController.h
@@ -0,0 +1,17 @@
+//
+//  PlaceResultViewController.h
+//  PushMe
+//
+//  Created by Aleksandr Fetisov on 15.03.2020.
+//  Copyright © 2020 Aleksandr Fetisov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlaceResultViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
